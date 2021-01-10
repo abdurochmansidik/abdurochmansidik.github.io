e8cbab56f29b2a9548b657854a9f2c611ac8e19b
abdurochmansidik <abdurochmansidik@gmail.com>
2021-01-11 06:04:31 +0700

    Updates

---
+++ precache-manifest.90dc1c70107b63a61a5747c2ffac6d36.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5bcde29bc220c2edbb443132003e55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4d41fa97605358910a2",
+    "url": "/static/css/3.877ede05.chunk.css"
+  },
+  {
+    "revision": "fbbdd6e6f8efe6320d4c",
+    "url": "/static/css/4.1dcd749f.chunk.css"
+  },
+  {
+    "revision": "8df58792727f062167e7",
+    "url": "/static/css/main.640cd646.chunk.css"
+  },
+  {
+    "revision": "6def15550d8e31abf0cd",
+    "url": "/static/js/2.67236a68.chunk.js"
+  },
+  {
+    "revision": "e4d41fa97605358910a2",
+    "url": "/static/js/3.02b3678f.chunk.js"
+  },
+  {
+    "revision": "fbbdd6e6f8efe6320d4c",
+    "url": "/static/js/4.9cd66a8e.chunk.js"
+  },
+  {
+    "revision": "8df58792727f062167e7",
+    "url": "/static/js/main.a48839ca.chunk.js"
+  },
+  {
+    "revision": "21b9aad5dcdeea095c45",
+    "url": "/static/js/runtime-main.189c368e.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "e31e705a242af4c6eb22ad357c4d49df",
+    "url": "/static/media/BLKDLogo.e31e705a.png"
+  },
+  {
+    "revision": "5298c026cfc9baad6bdd6e4f7304dee9",
+    "url": "/static/media/BNSP-logo.5298c026.png"
+  },
+  {
+    "revision": "5aefa0a9f091fbb5da28ae828ae816e6",
+    "url": "/static/media/CISCO-logo.5aefa0a9.png"
+  },
+  {
+    "revision": "187d5fb6f67fe45d47b5fc98e64347d6",
+    "url": "/static/media/LPIA-logo.187d5fb6.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "3e16ed825a3e8c71f0b180f4e6f0906f",
+    "url": "/static/media/PPKD-logo.3e16ed82.png"
+  },
+  {
+    "revision": "ab9a546c865d8001531c85eb5dd5b52e",
+    "url": "/static/media/PertanianLogo.ab9a546c.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "bf8f1e0e0e38018ea429279e76f537ef",
+    "url": "/static/media/contactMail.bf8f1e0e.svg"
+  },
+  {
+    "revision": "a983f138f3e13480d2b89222259fe2c1",
+    "url": "/static/media/developerActivity.a983f138.svg"
+  },
+  {
+    "revision": "c1130481efe57bc093faff25faee7fe5",
+    "url": "/static/media/developerActivity1.c1130481.svg"
+  },
+  {
+    "revision": "13ae2cbd484bf523a0c80039afb09d3c",
+    "url": "/static/media/developerActivity2.13ae2cbd.svg"
+  },
+  {
+    "revision": "a41f784840703e7c39e9d5ff37133c77",
+    "url": "/static/media/developerActivity3.a41f7848.svg"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "/static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "55ae4e44508bb9e9723acfc63e1c189e",
+    "url": "/static/media/mahadhikaLogo.55ae4e44.png"
+  },
+  {
+    "revision": "ddc179ee20f85401ab083dcc4235fc3f",
+    "url": "/static/media/manOnTable.ddc179ee.svg"
+  },
+  {
+    "revision": "a15c230b794fda4eb28f2320529072a1",
+    "url": "/static/media/manOnTable1.a15c230b.svg"
+  },
+  {
+    "revision": "25e00ca73ec1bd6636e1537c1a0c3a33",
+    "url": "/static/media/manOnTable2.25e00ca7.svg"
+  },
+  {
+    "revision": "f73380c8904730341d150ba163e5df37",
+    "url": "/static/media/meta_pic.f73380c8.png"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "3daa2a8511e013eceb6f56675877015b",
+    "url": "/static/media/programmer.3daa2a85.svg"
+  },
+  {
+    "revision": "1a60e8a9e2c2d6a539c7692f27fe6790",
+    "url": "/static/media/pwa.1a60e8a9.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "a489b49ed3ec7abd1dd93d8d9b044adb",
+    "url": "/static/media/skill.a489b49e.svg"
+  },
+  {
+    "revision": "1063e980bb408b5c463d2e808b69b31b",
+    "url": "/static/media/skill2.1063e980.svg"
+  },
+  {
+    "revision": "96c9d7a913fc06de9bd7e13f5cb26af9",
+    "url": "/static/media/skill3.96c9d7a9.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/static/media/talksCardBack.0288c5ec.svg"
+  },
+  {
+    "revision": "b48e86c9a956d3a3a32ca4a5ae05ddc7",
+    "url": "/static/media/talksCardBackAlt.b48e86c9.svg"
+  },
+  {
+    "revision": "424ef21b28e5975203ed4e58c813e370",
+    "url": "/static/media/unindraLogo.424ef21b.png"
+  }
+]);
